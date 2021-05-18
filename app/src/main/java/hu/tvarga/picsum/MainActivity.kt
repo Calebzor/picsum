fd1580099bda4a3cd6fadf4511c87a3628424247
Tamás Varga <mail@tvarga.hu>
2021-05-18 19:41:56 +0200

    initial import

---
+++ app/src/main/java/hu/tvarga/picsum/MainActivity.kt
@@ -0,0 +1,18 @@
+package hu.tvarga.picsum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hu.tvarga.picsum.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/hu/tvarga/picsum/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package hu.tvarga.picsum.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
